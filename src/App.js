d41bb21d56528cf3f78966af8541cb3ff882532c
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-01 00:17:41 -0400

    quick edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Title from "./Title";
+import DestinationBox from "./home/DestinationBox";
 import spaceship from './images/spaceship.png';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <div className="homecontent">
+      <div className="hero-section">
         <Title></Title>
         <div className="home-image">
           <div className="space-ship-wrapper">
@@ -15,6 +16,7 @@ function App() {
           </div>
         </div>
       </div>
+      <DestinationBox></DestinationBox>
     </div>
   );
 }
