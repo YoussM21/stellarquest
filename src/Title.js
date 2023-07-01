12038514fdf562564a20b8d8d00262d1c66f5837
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-06-30 22:39:45 -0400

    home title

---
+++ src/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className="box">
+      <div className="slogan-wrapper">
+        <div className="slogan">
+          <h1 className="text-wrapper">UNVEILING</h1>
+          <div className="div">UNIVERSE</div>
+          <div className="text-wrapper-2">THE</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
