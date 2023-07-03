d87a772a796981674a7cd291008af6460ecc4f48
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-02 20:37:24 -0400

    booking form code

---
+++ src/Destinations/Destinations.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookingForm from './BookingForm';
+import "./BookingFrom.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Destinations = () => {
+    return ( 
+        <div className="content-destinations">
+           <BookingForm />
+        </div>
+     );
+}
+ 
+export default Destinations;
