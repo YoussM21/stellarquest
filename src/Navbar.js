56d0f4b38ba9d73caf070ddc1555e0c5ec3f799f
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-12 12:23:10 -0400

    using link instead of a anchor tags

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,24 @@
-import logo from './images/logo.svg';
-import './Navbar.css'
+import logo from "./images/logo.svg";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (  
-        <nav className="navbar">
-            <div className="logo">
-                <a href = "/">
-                <img src={logo} className="" alt="logo" /></a>
-            </div>
-            <div className="links">
-                <a href="/destinations"> Destinations </a>
-                <a href="/potd"> POTD </a>
-                <a href="/news"> News </a>
-                <a href="/about"> About us </a>
-                <a href='/login'> Login </a>     
-            </div>
-        </nav>
-    );
-}
- 
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/">
+          <img src={logo} className="" alt="logo" />
+        </a>
+      </div>
+      <div className="Links">
+        <Link to="/destinations"> Destinations </Link>
+        <Link to="/potd"> POTD </Link>
+        <Link to="/news"> News </Link>
+        <Link href="/about"> About us </Link>
+        <Link href="/login"> Login </Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
