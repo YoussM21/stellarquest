a8be0e8024b5387676fa12d5cf7343ff0262dda5
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-11 23:29:23 -0400

    fix BrowseRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Home from "./Home";
