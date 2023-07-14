9db6cb4b9083611a5bd890e34d2db90f64e01a42
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-13 23:14:54 -0400

    num people in space api

--- src/PeopleInSpace.js
+++ src/PeopleInSpace.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import "./PeopleInSpace.css";
 import astronaut from "./images/astronaut.svg";
 import planetPOV from "./images/planetPOV.svg";
+import axios from 'axios';
 
 const PeopleInSpace = () => {
+
+  const [peopleCount, setPeopleCount] = useState(0);
+
+  useEffect(() => {
+    axios.get('http://api.open-notify.org/astros.json')
+      .then(response => {
+        setPeopleCount(response.data.number);
+      })
+      .catch(error => {
+        console.error('Error fetching space people count:', error);
+      });
+  }, []);
+
   return (
     <div className="Container">
       <div className="box-people">
         <div className="text-wrapper3">
           <div className="title-people">Number Of People In Space</div>
-          <div className="number">21</div>
+          <div className="number"> {peopleCount} </div>
         </div>
       </div>
       <div className="image-container2">
