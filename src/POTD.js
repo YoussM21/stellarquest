0c05d8c0c5243657b931c109a65baae27ba6b3ec
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-19 17:12:32 -0400

    Pic of the day API

--- src/POTD.js
+++ src/POTD.js
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./POTD.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button } from "react-bootstrap";
-import galaxypic from './images/galaxy-pic.svg';
 
 const POTD = () => {
+  const [date, setDate] = useState("");
+  const [picData, setPicData] = useState(null);
+
+  const fetchPicOfTheDay = async () => {
+    const response = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=SxNAsbFbGe2X6cTOR5Ee0t0cf5V9WCnbQ3cNXj55&date=${date}`
+    );
+    setPicData(response.data);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchPicOfTheDay();
+  };
+
   return (
     <div className="POTD-container">
       <div className="POTD-title">Picture Of The Day</div>
       <div className="POTD-wrapper">
-        <img src={galaxypic} alt="" className="galaxy-pic" />
+        {picData && (
+          <img src={picData.url} alt={picData.title} className="generated-pic" />
+        )}
         <div className="POTD-text-wrapper">
           <div className="Date-title">Choose The Date</div>
           <div className="date-input">
-            <Form.Group className="mb-3" controlId="Date">
-              <Form.Control
-                type="date"
-                placeholder="Select Your Date"
-                className="custom-input"
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Search
-            </Button>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="Date">
+                <Form.Control
+                  type="date"
+                  placeholder="Select Your Date"
+                  className="custom-input"
+                  value={date}
+                  onChange={handleDateChange}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Search
+              </Button>
+            </Form>
           </div>
-          <div className="pic-description">
-            Lorem ipsum dolor sit amet, consectetur adipisc Lorem ipsum dolor
-            sit amet, consectetur adipiscLorem ipsum dolor sit amet, consectetur
-            adipiscLorem ipsum dolor sit amet, consectetur adipisc Lorem ipsum
-            dolor sit amet, consectetur adipisc Lorem ipsum dolor sit amet,
-            consectetur Lorem ipsum dolor sit amet, consectetur adipisc Lorem
-            ipsum dolor sit amet, consectetur adipiscLorem ipsum dolor sit amet,
-            consectetur adipiscLorem ipsum dolor sit amet, consectetur adipisc
-            Lorem ipsum dolor sit amet, consectetur adipisc Lorem ipsum dolor
-            sit amet, consectetur dolor sit amet, consectetur adipiscLorem ipsum dolor sit amet,
-            consectetur adipiscLorem ipsum dolor sit amet, consectetur adipisc
-            Lorem ipsum dolor sit amet, consectetur adipisc Lorem ipsum dolor
-            sit amet, consectetur
-          </div>
+          {picData && (
+            <div className="pic-description">
+              <h2 className="pic-title">{picData.title}</h2>
+              <p>{picData.explanation}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
