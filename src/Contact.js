e9edd5677cad97a87d37d9bbe96a44cb9490dad5
Youssef Mroue <mroueh.youssef10@gmail.com>
2023-07-23 19:35:10 -0400

    fixing button link for contact us page

--- src/Contact.js
+++ src/Contact.js
@@ -1,75 +1,10 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import FloatingLabel from "react-bootstrap/FloatingLabel";
-// import Form from "react-bootstrap/Form";
-// import "./Contact.css";
-
-// const Contact = ({ language, contactTranslations }) => {
-//   const translations =
-//     language === "fr" ? contactTranslations.fr : contactTranslations.en;
-
-//   return (
-//     <div className="contact-form">
-//       <Form>
-//         <FloatingLabel
-//           controlId="fullName"
-//           label={translations.fullNameLabel}
-//           className="mb-3"
-//         >
-//           <Form.Control
-//             type="text"
-//             placeholder={translations.fullNamePlaceholder}
-//           />
-//         </FloatingLabel>
-
-//         <FloatingLabel
-//           controlId="email"
-//           label={translations.emailLabel}
-//           className="mb-3"
-//         >
-//           <Form.Control
-//             type="email"
-//             placeholder={translations.emailPlaceholder}
-//           />
-//         </FloatingLabel>
-
-//         <FloatingLabel
-//           controlId="messageTitle"
-//           label={translations.messageTitleLabel}
-//           className="mb-3"
-//         >
-//           <Form.Control
-//             type="text"
-//             placeholder={translations.messageTitlePlaceholder}
-//           />
-//         </FloatingLabel>
-
-//         <FloatingLabel
-//           controlId="messageContent"
-//           label={translations.messageContentLabel}
-//           className="mb-3"
-//         >
-//           <Form.Control
-//             as="textarea"
-//             placeholder={translations.messageContentPlaceholder}
-//             style={{ height: "150px" }}
-//           />
-//         </FloatingLabel>
-//         <Link to = "/PageNotFound">
-//           <button className="button1">{translations.sendMessageButton}</button>
-//         </Link>
-//       </Form>
-//     </div>
-//   );
-// };
-
-// export default Contact;
 import React from "react";
 import { Formik, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 import "./Contact.css";
 
 const Contact = ({ language, contactTranslations }) => {
@@ -88,7 +23,6 @@ const Contact = ({ language, contactTranslations }) => {
     <Formik
       validationSchema={schema}
       onSubmit={(values) => {
-        // Your form submission logic here
         console.log(values);
       }}
       initialValues={{
@@ -181,9 +115,11 @@ const Contact = ({ language, contactTranslations }) => {
             />
           </FloatingLabel>
 
-          <Button type="submit" className="button1">
-            {translations.sendMessageButton}
-          </Button>
+          <Link to = "/PageNotFound">
+            <Button type="submit" className="button1">
+              {translations.sendMessageButton}
+            </Button>
+          </Link>
         </Form>
       )}
     </Formik>
